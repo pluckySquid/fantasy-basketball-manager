cde200bca96e7ae88bf06d59d94a1ce5a65e342b
Yunshu <yunshuwang95@gmail.com>
2026-03-29 13:33:29 -0500

    Remove generated artifacts

--- .build/dev/static/chunks/src_app_page_tsx_0-0lnb2._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_13k8in1._.js"],
-    source: "dynamic"
-});
--- .build/server/chunks/ssr/_next-internal_server_app_schedule_page_actions_0h~rr-e.js
+++
@@ -1,3 +0,0 @@
-module.exports=[78495,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_schedule_page_actions_0h~rr-e.js.map
